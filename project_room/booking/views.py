145b6ef5ceb98da3b2e4333469cb1a53347807ba
cnaut88 <belastanislav69@gmail.com>
2025-10-25 10:22:00 +0300

    third part of project

--- project_room/booking/views.py
+++ project_room/booking/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render, redirect
 from .models import Room, Booking
 from .forms import BookingForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,authenticate
 from django.contrib import messages
-
 def home(request):
     rooms = Room.objects.all()
     return render(request, "home.html", {"rooms": rooms})
 
+
+def login_register(request):
+    login_form = AuthenticationForm()
+    register_form = UserCreationForm()
+
+    if request.method == 'POST':
+        if 'login' in request.POST:
+            login_form = AuthenticationForm(request, data=request.POST)
+            if login_form.is_valid():
+                user = login_form.get_user()
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Невірний логін або пароль")
+
+        elif 'register' in request.POST:
+            register_form = UserCreationForm(request.POST)
+            if register_form.is_valid():
+                user = register_form.save()
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Помилка реєстрації. Спробуйте ще раз.")
+
+    return render(request, 'registration/login_register.html', {
+        'login_form': login_form,
+        'register_form': register_form
+    })
 
 def book_room(request):
     if request.method == "POST":
@@ -34,3 +63,4 @@ def book_room(request):
     else:
         form = BookingForm()
     return render(request, "book_room.html", {"form": form})
+
--- project_room/room/settings.py
+++ project_room/room/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
     }
 }
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
